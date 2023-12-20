eed772ac6a572571bde5b0498698a4f26fb6fd09
zad-amohammad <amohammad@zadsolutions.com>
2023-12-20 13:27:01 +0200

    fix stock move dimension

--- models/stock_move.py
+++ models/stock_move.py
@@ -3,5 +3,5 @@ from odoo import fields, models
 class StockMove(models.Model):
     _inherit = "stock.move"
 
-    dimension = fields.Char(related="sale_line_id.dimension")
+    dimension = fields.Char(related="sale_line_id.dimension", store=True)
 
