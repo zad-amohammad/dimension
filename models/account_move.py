1394d01554e5b4a7b0920b51724666e288f3e41d
zad-amohammad <amohammad@zadsolutions.com>
2023-12-20 13:48:40 +0200

    fix AccountMoveLine to get from last none cancelld delivary

--- models/account_move.py
+++ models/account_move.py
@@ -8,9 +8,12 @@ class AccountMoveLine(models.Model):
     @api.depends('account_id')
     def _compute_order(self):
         dim = None
-        if self.move_id.line_ids.sale_line_ids.order_id.picking_ids:
-            for move in self.move_id.line_ids.sale_line_ids.order_id.picking_ids[0].move_ids:
-                if move.product_id.id == self.product_id.id:
-                    dim = move.dimension
+        pickings =self.move_id.line_ids.sale_line_ids.order_id.picking_ids
+        if pickings:
+            pickings = [ picking for picking in pickings if picking.state != 'cancel' ]
+            if pickings:
+                for move in pickings[-1].move_ids:
+                    if move.product_id.id == self.product_id.id:
+                        dim = move.dimension
         self.dimension = dim
     
